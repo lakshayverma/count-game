7226f966867c778f1202c00ee83a5c5d6b1787bf
Lakshay Verma <verma_lakshay@live.in>
2018-04-01 21:47:06 +0530

    Setup folder structure

---
+++ scripts/game-es6.js
@@ -0,0 +1,14 @@
+// logic for thr count game
+
+class Game {
+    constructor() {
+        console.log(`Welcome to the game. Version ${this.version()}`);
+    }
+
+    version() {
+        return '1.0.0';
+    }
+}
+
+
+var game = new Game();
