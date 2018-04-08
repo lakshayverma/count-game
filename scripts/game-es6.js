b9b146004b5b7727342f4a2320557937320bf207
Lakshay Verma <verma_lakshay@live.in>
2018-04-08 19:17:55 +0530

    Include Create JS 2015 into Canvas logic

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,14 +1,27 @@
-// logic for thr count game
+// logic for the Count game
 
 class Game {
-    constructor() {
-        console.log(`Welcome to the game. Version ${this.version()}`);
-    }
+  constructor() {
+    console.log(`Welcome to the game. Version ${this.version()}`);
 
-    version() {
-        return '1.0.0';
-    }
+    this.canvas = document.getElementById("game-canvas");
+    this.stage = new createjs.Stage(this.canvas);
+
+    createjs.Ticker.setFPS(60);
+
+    // Keep re-drawing the stage.
+    createjs.Ticker.on("tick", this.stage);
+
+    // Testing code
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("gold").drawCircle(0, 0, 40);
+    circle.x = circle.y = 100;
+    this.stage.addChild(circle);
+  }
+  version() {
+    return '1.0.0';
+  }
 }
 
-
+// start the game
 var game = new Game();
