573e73693d1d7f6b97c374f6c0256661e1d871b3
Lakshay Verma <verma_lakshay@live.in>
2018-04-08 19:37:57 +0530

    Use Graphics on Canvas

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,5 +1,20 @@
 // logic for the Count game
 
+class NumberedBox extends createjs.Container {
+  constructor (number = 0) {
+    super();
+
+    var movieClip = new lib.NumberedBox();
+    movieClip.numberText.text = number;
+
+    this.addChild(movieClip);
+
+    // random position
+    movieClip.x = Math.random() * 200;
+    movieClip.y = Math.random() * 200;
+  }
+}
+
 class Game {
   constructor() {
     console.log(`Welcome to the game. Version ${this.version()}`);
@@ -7,16 +22,18 @@ class Game {
     this.canvas = document.getElementById("game-canvas");
     this.stage = new createjs.Stage(this.canvas);
 
+    window.debugStage = this.stage;
+
     createjs.Ticker.setFPS(60);
 
     // Keep re-drawing the stage.
     createjs.Ticker.on("tick", this.stage);
 
+    // background
+    this.stage.addChild(new lib.Background());
+
     // Testing code
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("gold").drawCircle(0, 0, 40);
-    circle.x = circle.y = 100;
-    this.stage.addChild(circle);
+    this.stage.addChild(new NumberedBox(88));
   }
   version() {
     return '1.0.0';
